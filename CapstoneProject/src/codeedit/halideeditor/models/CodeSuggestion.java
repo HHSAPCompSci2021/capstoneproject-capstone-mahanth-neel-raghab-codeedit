dffd3c01f80e88f392c45131f985483993ec5720
Mahanth Mohan <mohan.mahanth@gmail.com>
2022-05-20 12:14:31 -0700

    some changes

--- CapstoneProject/src/codeedit/halideeditor/models/CodeSuggestion.java
+++ CapstoneProject/src/codeedit/halideeditor/models/CodeSuggestion.java
@@ -1,56 +1,51 @@
 package codeedit.halideeditor.models;
 
 public class CodeSuggestion implements Comparable<CodeSuggestion> {
-	//0 = field, 1 = method
+	// 0 = field, 1 = method
 	private int type;
-    private String name;
-   	private String[] params;
-	private String[] returnValues;
+	private String name;
 
 	public CodeSuggestion(String name, int type) {
 		this.name = name;
-		this.type=type;
+		this.type = type;
 	}
 
 	public CodeSuggestion(String name, String[] params, String[] returnValues) {
 		this.name = name;
-		this.params = params;
-		this.returnValues = returnValues;
 		type = 1;
 	}
 
-    public String getName() {
+	public String getName() {
 		return name;
 	}
 
-    public String[] getParams() {
+	public String[] getParams() {
 		return params;
 	}
 
-	public String[] getReturnValues() {
-		return returnValues;
-	}
 	@Override
 	public String toString() {
 		return name;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		CodeSuggestion o = (CodeSuggestion) obj;
-		/*TODO
+		/*
+		 * TODO
 		 * Make work properly with name, params, and returnValues
 		 */
-		
-		if (o.name.equals(name)&&o.params.equals(params))
+
+		if (o.name.equals(name))
 			return true;
 		return false;
 	}
-	
+
 	@Override
 	public int compareTo(CodeSuggestion o) {
 		return this.name.compareTo(o.name);
 	}
-	
+
 	public int getType() {
 		return type;
 	}
