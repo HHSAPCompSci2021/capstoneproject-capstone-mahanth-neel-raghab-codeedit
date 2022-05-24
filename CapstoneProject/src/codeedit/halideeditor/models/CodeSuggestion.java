58b0528e43aa252b81c069f5f959f41c16bf7e65
Neel Sudhakaran <89160678+nsudhakaran679@users.noreply.github.com>
2022-05-24 11:31:46 -0700

    More JavaDocs

--- CapstoneProject/src/codeedit/halideeditor/models/CodeSuggestion.java
+++ CapstoneProject/src/codeedit/halideeditor/models/CodeSuggestion.java
@@ -1,19 +1,41 @@
 package codeedit.halideeditor.models;
 
+/**
+ * Represents a Code Suggestion
+ * @author Mahanth Mohan
+ */
 public class CodeSuggestion implements Comparable<CodeSuggestion> {
+	
+	/**
+	 * Name of the code suggestion.
+	 */
 	private String name;
 	// 0 = field, 1 = method, 2 keyword
+	
+	/**
+	 * Type of the code suggestion.
+	 */
 	private int type;
 
+	/**
+	 * Creates a new code suggestion.
+	 * @param name the suggestion name
+	 */
 	public CodeSuggestion(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Creates a new code suggestion.
+	 * @param name the suggestion name
+	 * @param type the suggestion type
+	 */
 	public CodeSuggestion(String name, int type) {
 		this.name = name;
 		this.type = type;
 	}
 
+	@Override
 	public String toString() {
 		return name;
 	}
@@ -23,6 +45,7 @@ public class CodeSuggestion implements Comparable<CodeSuggestion> {
 		return name.compareTo(o.name);  
 	}
 
+	@Override
 	public boolean equals(Object o) {
 		if (o instanceof CodeSuggestion) {
 			CodeSuggestion other = (CodeSuggestion) o;
@@ -32,10 +55,19 @@ public class CodeSuggestion implements Comparable<CodeSuggestion> {
 		return false;
 	}
 
+	
+	/**
+	 * Gets the name of the suggestion
+	 * @return the name of the suggestion.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Gets the type of the suggestion.
+	 * @return the type of the suggestion.
+	 */
 	public int getType() {
 		return type;
 	}
