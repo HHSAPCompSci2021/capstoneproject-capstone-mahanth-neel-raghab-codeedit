f57776bf578d14d824073f6b14da80e1d4f126fd
Raghab-B <rbaruah169@student.fuhsd.org>
2022-05-24 11:19:01 -0700

    javadocs

--- CapstoneProject/src/codeedit/halideeditor/utils/CodeDict.java
+++ CapstoneProject/src/codeedit/halideeditor/utils/CodeDict.java
@@ -1,8 +1,15 @@
 package codeedit.halideeditor.utils;
 
 import java.util.ArrayList;
-
+/**
+ * a class that contains the java keywords, methods, fields, and custom code snippets
+ * @author Raghab Baruah
+ *
+ */
 public class CodeDict {
+	/**
+	 * an array of all the java keywords
+	 */
 	public static final String[] JAVA_KEYWORDS = { "abstract", "assert", "boolean",
 			"break", "byte", "case", "catch", "char", "class", "const",
 			"continue", "default", "do", "double", "else", "enum", "extends", "false",
@@ -12,8 +19,16 @@ public class CodeDict {
 			"return", "short", "static", "strictfp", "super", "switch",
 			"synchronized", "this", "throw", "throws", "transient", "true",
 			"try", "void", "volatile", "while" };
-
+	/**
+	 * An array of the methods in the code
+	 */
 	public static ArrayList<String> METHODS = new ArrayList<>();
+	/**
+	 * An array of the fields in the code
+	 */
 	public static ArrayList<String> FIELDS = new ArrayList<>();
+	/**
+	 * An array of the custom snippets
+	 */
 	public static ArrayList<String> SNIPPETS = new ArrayList<>();
 }
